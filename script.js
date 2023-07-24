e3680c9dc81721b44b1b797419fd755777d99e5e
Po Lien Lin <polien.joe.lin@gmail.com>
2023-07-24 22:53:17 +0800

    Plan the API building Order

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ app.listen(3000, ()=>{
     console.log("This app is running on port 3000.");
 });
 
+//  API Building Order
+/*
+--> res = this is working
+/signin --> POST = success or fail
+/register --> POST = user
+/profile/:userID --> GET = user
+/image --> PUT --> user
+*/
