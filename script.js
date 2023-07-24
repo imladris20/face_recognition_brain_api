9dd2422da2dc6109affb740a5d8dec326fc94999
Po Lien Lin <polien.joe.lin@gmail.com>
2023-07-25 00:10:09 +0800

    Build sign in function

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const database = {
+    user: [
+        {
+            id:'123',
+            name: 'Polien',
+            email: 'polien@gmail.com',
+            password: 'love0313',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id:'124',
+            name: 'Carpenter',
+            email: 'carpenter@gmail.com',
+            password: 'love2950',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/',(req,res) => {
     res.send("This is working!");
+})
+
+app.post('/signin', (req,res) => {
+    if (req.body.email === database.user[0].email &&
+        req.body.password === database.user[0].password ){
+        res.json('log in success');
+    } else {
+        res.status(400).json('error logging in');
+    }
 })
 
 app.listen(3000, ()=>{
